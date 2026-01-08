eb91b0379f4f7b5bd5c52eb7836d17c3ae5b15db
sahilshaikh2023 <sahilshaikh6922@gmail.com>
2026-01-08 21:39:23 +0530

    3rd queation

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-// print the number of count when enter the yes 
+// print the number bwn 1 to  50 divisible by the 7
 
-let word = prompt("Enter the word");
 
-let counter = 0;
+// for ( let i = 1 ; i<=50 ; i++ ){
 
-while(word !== "stop"){
-    if(word=="yes") counter++;
-    word =prompt("Enter the word ");
+//     if (i%7 == 0){
+//         console.log(i);
+//     }
+// }
 
+
+// sum of all odd number
+// let sum = 0;
+
+
+// for (let i =1 ; i<31 ; i++)
+// {
+//     if (i%2 !=0){
+
+//         sum = sum+i;
+//     }
+// }
+// console.log(sum);
+
+
+// keep asking user to enter the even numeber
+
+
+// let user = +prompt("Enter the even number");
+// while(user%2!==0){
+//     user = +prompt("Enter the even number");
+// }
+
+
+// console.log(`you enterd the even number ${user}`);
+
+
+let m = +prompt("Enter the starting number");
+
+let k = +prompt("Enter the last number ");
+
+for (let i = m; i<=k; i++ ){
+    console.log(i);
 }
-
-console.log(`the yes count is ${counter}`);
